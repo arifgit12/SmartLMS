9eee5c86011a817e2ecb55b962f560f4e27371f1
ARIF MONDAL <ARIF MONDAL>
2017-02-15 22:43:58 +0300

    Quiz model restructure

--- SmartLMS/Models/Quiz.cs
+++ SmartLMS/Models/Quiz.cs
@@ -13,11 +13,9 @@ namespace SmartLMS.Models
         public int QuizId { get; set; }
         [Required]
         public string QuizName { get; set; }
-        public ICollection<Question> Question { get; set; }
-        public ICollection<StudentQuiz> Students { get; set; }
         public int CourseId { get; set; }
         public Course Course { get; set; }
-       
+
         [Required]
         public DateTime? StartTime { get; set; }
         [Required]
@@ -26,5 +24,8 @@ namespace SmartLMS.Models
         public DateTime? EndTime { get; set; }
         [Required]
         public int Score { get; set; }
+
+        public ICollection<Question> Question { get; set; }
+        public ICollection<StudentQuiz> Students { get; set; }
     }
 }
