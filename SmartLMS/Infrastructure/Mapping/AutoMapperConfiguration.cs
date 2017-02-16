6d60a7724cb3efb108019fab3ae18f5ab9674e6c
ARIF MONDAL <ARIF MONDAL>
2017-02-16 19:09:53 +0300

    AutoMapper configuration added

---
+++ SmartLMS/Infrastructure/Mapping/AutoMapperConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using SmartLMS.Models;
+using SmartLMS.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SmartLMS.Infrastructure.Mapping
+{
+    public class AutoMapperConfiguration
+    {
+        public static void RegisterMaps()
+        {
+            AutoMapper.Mapper.Initialize(config => 
+            {
+                config.CreateMap<Category, CategoryViewModel>();
+                config.CreateMap<CategoryViewModel, Category>();
+            });
+        }
+    }
+}
