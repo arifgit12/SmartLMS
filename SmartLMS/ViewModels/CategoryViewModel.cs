e1987db44d70da2931e848e2641177dd117d5286
ARIF MONDAL <ARIF MONDAL>
2017-02-16 19:14:17 +0300

    Category Controller and View updated

---
+++ SmartLMS/ViewModels/CategoryViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace SmartLMS.ViewModels
+{
+    public class CategoryViewModel
+    {
+        [Display(Name = "Category")]
+        public int CategoryId { get; set; }
+
+        [Required]
+        [Display(Name = "Category Name")]
+        public string CategoryName { get; set; }
+    }
+}
